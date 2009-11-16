aa9d09d60534ca79f1ad34c0d5a3f04c88f771d0
pablo.brusco <pablo.brusco@c7fe8a42-c490-11de-81ce-f10158adbdd1>
2009-11-16 17:16:03 +0000

    atendida las interr

--- trunk/codigo/isr.asm
+++ trunk/codigo/isr.asm
@@ -8,33 +8,208 @@ extern pic1_intr_end
 ; TODO: Definir el resto de las ISR
 ; ----------------------------------------------------------------
 
-global _isr0, _isr20, _isr21
+global _isr0, _isr1, _isr2, _isr3, _isr4, _isr5, _isr6, _isr7, _isr8, _isr9, _isrA, _isrB, _isrC, _isrD, _isrE, _isrF, _isr10, _isr11, _isr12, _isr13, _isr20, _isr21
 
-msgisr0: db 'EXCEPCION #DE: Error de division'
+
+msgisr0: db 'HOLA MUNDO!! ehhhhhh digo.. dividiste por cero, sos un zoquete! eso te pasa por andar tocando lo que no debes.. si, a vos te digo.. cual es tu problema? eh? no.. no apretes ctrl c .. Noooooooooooooo!!!!!!!!!'
 msgisr0_len equ $-msgisr0
+
+msgisr1: db '1'
+msgisr1_len equ $-msgisr1
+
+msgisr2: db '2'
+msgisr2_len equ $-msgisr2
+
+msgisr3: db '3'
+msgisr3_len equ $-msgisr3
+
+msgisr4: db '4'
+msgisr4_len equ $-msgisr4
+
+msgisr5: db '5'
+msgisr5_len equ $-msgisr5
+
+msgisr6: db '6'
+msgisr6_len equ $-msgisr6
+
+msgisr7: db '7'
+msgisr7_len equ $-msgisr7
+
+msgisr8: db '8'
+msgisr8_len equ $-msgisr8
+
+
+msgisr9: db '9'
+msgisr9_len equ $-msgisr9
+
+msgisrA: db 'A'
+msgisrA_len equ $-msgisrA
+
+msgisrB: db 'B'
+msgisrB_len equ $-msgisrB
+
+msgisrC: db 'C'
+msgisrC_len equ $-msgisrC
+
+msgisrD: db 'D'
+msgisrD_len equ $-msgisrD
+
+msgisrE: db 'E'
+msgisrE_len equ $-msgisrE
+
+msgisrF: db 'F'
+msgisrF_len equ $-msgisrF
+
+msgisr10: db '10'
+msgisr10_len equ $-msgisr10
+
+msgisr11: db '11'
+msgisr11_len equ $-msgisr11
+
+msgisr12: db '12'
+msgisr12_len equ $-msgisr12
+
+msgisr13: db '13'
+msgisr13_len equ $-msgisr13
 
 
 
 msgisr20: db 'Interrupcion : Timer tick'
 msgisr20_len equ $-msgisr20
 
-msgisr21: db 'Interrupcion : Tecleastesssssss'
+msgisr21: db 'Interrupcion : Tecleaste Algo'
 msgisr21_len equ $-msgisr21
+
+
 
 _isr0:
 	cli
 	mov edx, msgisr0
-	IMPRIMIR_TEXTO edx, msgisr0_len, 0x0C, 0, 0, 0x13000
+	IMPRIMIR_TEXTO edx, msgisr0_len, 0xEC, 10, 0, 0x13000
 	jmp $
 	
+_isr1:
+	cli
+	mov edx, msgisr1
+	IMPRIMIR_TEXTO edx, msgisr1_len, 0x0C, 0, 0, 0x13000
+	jmp $
 
+_isr2:
+	cli
+	mov edx, msgisr2
+	IMPRIMIR_TEXTO edx, msgisr2_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isr3:
+	cli
+	mov edx, msgisr3
+	IMPRIMIR_TEXTO edx, msgisr3_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isr4:
+	cli
+	mov edx, msgisr4
+	IMPRIMIR_TEXTO edx, msgisr4_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isr5:
+	cli
+	mov edx, msgisr5
+	IMPRIMIR_TEXTO edx, msgisr5_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isr6:
+	cli
+	mov edx, msgisr6
+	IMPRIMIR_TEXTO edx, msgisr6_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isr7:
+	cli
+	mov edx, msgisr7
+	IMPRIMIR_TEXTO edx, msgisr7_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isr8:
+	cli
+	mov edx, msgisr8
+	IMPRIMIR_TEXTO edx, msgisr8_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isr9:
+	cli
+	mov edx, msgisr9
+	IMPRIMIR_TEXTO edx, msgisr9_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isrA:
+	cli
+	mov edx, msgisrA
+	IMPRIMIR_TEXTO edx, msgisrA_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isrB:
+	cli
+	mov edx, msgisrB
+	IMPRIMIR_TEXTO edx, msgisrB_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isrC:
+	cli
+	mov edx, msgisrC
+	IMPRIMIR_TEXTO edx, msgisrC_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isrD:
+	cli
+	mov edx, msgisrD
+	IMPRIMIR_TEXTO edx, msgisrD_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+
+_isrE:
+	cli
+	mov edx, msgisrE
+	IMPRIMIR_TEXTO edx, msgisrE_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isrF:
+	cli
+	mov edx, msgisrF
+	IMPRIMIR_TEXTO edx, msgisrF_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isr10:
+	cli
+	mov edx, msgisr10
+	IMPRIMIR_TEXTO edx, msgisr10_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isr11:
+	cli
+	mov edx, msgisr11
+	IMPRIMIR_TEXTO edx, msgisr11_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isr12:
+	cli
+	mov edx, msgisr12
+	IMPRIMIR_TEXTO edx, msgisr12_len, 0x0C, 0, 0, 0x13000
+	jmp $
+
+_isr13:
+	cli
+	mov edx, msgisr13
+	IMPRIMIR_TEXTO edx, msgisr13_len, 0x0C, 0, 0, 0x13000
+	jmp $
 
 
 _isr20: 
 	cli
-	call next_clock
 
 	;vamos a avisar que se atendio vio..
+	call next_clock	
+	
 	mov al, 0x20
 	out 0x20, al
 
@@ -45,16 +220,16 @@ _isr20:
 
 _isr21: 
 	cli
-	mov edx, msgisr21
-	IMPRIMIR_TEXTO edx, msgisr21_len, 0x0C, 0xA, 1, 0x13000
 
-	xor eax, eax
-	in al, 0x60
-
-	IMPRIMIR_TEXTO eax, 1, 0x0C, 0xA, (msgisr21_len+4), 0x13000
-
+	in al,0x60
 	mov al, 0x20
 	out 0x20, al
+	
+	mov edx, pantallazo
+	IMPRIMIR_TEXTO edx, 27, 0x3C, 1 , 1, 0x13000
+		
+	xor ax,ax
+	div ax	
 
 	sti
 	iret
@@ -77,10 +252,13 @@ next_clock:
 		IMPRIMIR_TEXTO ebx, 1, 0x0A, 23, 8, 0x13000
 	popad
 	ret
-
-isrmessage: db 'Clock:'
+		
+isrmessage: db 'Relos:'
 isrnumero: dd 0x00000000
 isrmessage1: db '|'
 isrmessage2: db '/'
 isrmessage3: db '-'
 isrmessage4: db '\'
+
+pantallazo: db '****** ****** ****** ******'
+
--- trunk/codigo/isr.h
+++ trunk/codigo/isr.h
@@ -6,7 +6,25 @@
  * TODO: Agregar el resto de las ISR
  */
 void _isr0();
-
+void _isr1();
+void _isr2();
+void _isr3();
+void _isr4();
+void _isr5();
+void _isr6();
+void _isr7();
+void _isr8();
+void _isr9();
+void _isrA();
+void _isrB();
+void _isrC();
+void _isrD();
+void _isrE();
+void _isrF();
+void _isr10();
+void _isr11();
+void _isr12();
+void _isr13();
 void _isr20();
 void _isr21();
 
